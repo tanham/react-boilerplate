44535748856d6a4a41d3a8166190704121852ae9
Tanisha <tanishahampden@gmail.com>
2018-06-09 15:59:15 -0700

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -7,5 +7,25 @@ module.exports = {
     output: {
         filename: '[name].[hash].js',
         path: path.resolve('./dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: ['node_modues'],
+                use: [{ loader: 'babel-loader'}],
+            },
+            {
+                test: /\.s(a|c)ss$/,
+                use: [{
+                    loader: 'style-loader'
+                }, {
+                    loader: 'css-loader'
+                }, {
+                    loader: 'sass-loader'
+                }], 
+            }
+            
+        ]
     }
 }
