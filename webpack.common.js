77e461170e7dd849cf61ead12834d81c7fd6441f
Tanisha <tanishahampden@gmail.com>
2018-06-09 16:49:04 -0700

    add dev and prod envs to webpack

--- webpack.common.js
+++ webpack.common.js
@@ -36,10 +36,5 @@ module.exports = {
             template: 'index.html'
         }),
         new CleanWebpackPlugin(['dist'])
-    ],
-    devServer: {
-        host: 'localhost',
-        port: 3000,
-        open: true,
-    }
+    ]
 }
